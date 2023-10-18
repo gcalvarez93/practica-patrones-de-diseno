90630c825362c68468c3d2c2de0020369ae54dde
Gabriel Castro <gcalvarez93@gmail.com>
2023-10-18 19:24:19 +0200

    fix bug project

---
+++ AppPatronesGabriel/AppPatronesGabriel/UI/Detail/Model/DetailModel.swift
@@ -0,0 +1,13 @@
+//
+//  DetailModel.swift
+//  AppPatronesGabriel
+//
+//  Created by Gabriel Castro on 15/10/23.
+//
+
+import Foundation
+
+struct DetailModel {
+    var homeCellModel: HomeCellModel
+    var description: String
+}
---
+++ AppPatronesGabriel/AppPatronesGabriel/UI/Home/Model/HomeCellModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeCellModel.swift
+//  AppPatronesGabriel
+//
+//  Created by Gabriel Castro on 15/10/23.
+//
+
+import Foundation
+
+struct HomeCellModel {
+    var name: String
+    var image: String?
+}
